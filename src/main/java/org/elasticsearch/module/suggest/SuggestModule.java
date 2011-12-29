978949e7022b0939a687bd9f9aea3fb1c8c9817d
Alexander Reelsen <alexander@reelsen.net>
2011-12-29 16:06:12 +0100

    Adding transport actions as eager singletons

--- src/main/java/org/elasticsearch/module/suggest/SuggestModule.java
+++ src/main/java/org/elasticsearch/module/suggest/SuggestModule.java
@@ -1,12 +1,15 @@
 package org.elasticsearch.module.suggest;
 
+import org.elasticsearch.action.suggest.TransportNodesSuggestRefreshAction;
+import org.elasticsearch.action.suggest.TransportSuggestAction;
 import org.elasticsearch.common.inject.AbstractModule;
 import org.elasticsearch.service.suggest.Suggester;
 
 public class SuggestModule extends AbstractModule {
 
     @Override protected void configure() {
+        bind(TransportSuggestAction.class).asEagerSingleton();
+        bind(TransportNodesSuggestRefreshAction.class).asEagerSingleton();
         bind(Suggester.class).asEagerSingleton();
-
     }
 }
